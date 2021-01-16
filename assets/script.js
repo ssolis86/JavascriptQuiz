373c9da2c84990b0554a7ddb158b9aafe5d012ad
Stephen Solis <ssolis8619@gmail.com>
2021-01-15 18:28:17 -0600

    added setInterval function to set timer.  Linked timer to Submit button

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 //Assignment Code
 var generateBtn = document.querySelector("#button");
-
+var timerEl = document.getElementById("countdown");
 
 function startQuiz(){
    
@@ -9,10 +9,16 @@ function startQuiz(){
 
 function nextPage() {
     var password = startQuiz();
-  console.log("hello")
 
+    var timeLeft = 3;
+    var timeInterval = setInterval(function() {
+      
+    if(timeLeft !== 0){
+        --timeLeft;
+        timerEl.textContent = timeLeft + " seconds remaining";
+    }
+
+    }, 1000);
   }
 
 generateBtn.addEventListener("click",nextPage);
-
-
