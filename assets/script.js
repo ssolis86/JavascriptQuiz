ede6c82a9eb7a08bcaf0b18ae9d253392ae1b1c2
Stephen Solis <ssolis8619@gmail.com>
2021-01-17 20:19:02 -0600

    added replaceWith function to switch out content in the card and created buttons for multiple choice options

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,11 @@
 var generateBtn = document.querySelector("#button");
 var timerEl = document.getElementById("countdown");
 
-function startQuiz(){
+
+
+
+function initiateQuiz() {
    
-
-}
-
-function nextPage() {
-    var password = startQuiz();
-
     var timeLeft = 3;
     var timeInterval = setInterval(function() {
       
@@ -17,8 +14,37 @@ function nextPage() {
         --timeLeft;
         timerEl.textContent = timeLeft + " seconds remaining";
     }
-
+    
     }, 1000);
+
+    
+    var questionOne = document.createElement("p");
+    var buttonOptions = document.createElement("p");
+    var optionA = document.createElement("BUTTON");
+    var optionB = document.createElement("BUTTON");
+    var optionC = document.createElement("BUTTON");
+    var optionD = document.createElement("BUTTON");
+
+    questionOne.textContent = "This is questions one.";
+    
+    optionA.innerHTML = "A: ";
+    optionB.innerHTML = "B: ";
+    optionC.innerHTML = "C: ";
+    optionD.innerHTML = "D: ";
+    
+    buttonOptions.appendChild(optionA);
+    buttonOptions.appendChild(optionB);
+    buttonOptions.appendChild(optionC);
+    buttonOptions.appendChild(optionD);
+
+    document.getElementById("buttonS").replaceWith(buttonOptions);
+    document.getElementById("codingQuiz").replaceWith(questionOne);
+    
+
+
+
+
+
   }
 
-generateBtn.addEventListener("click",nextPage);
+generateBtn.addEventListener("click",initiateQuiz);
