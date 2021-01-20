068ff7929b233a97a0bf3e2e4ffe4ead89aeeb6a
Stephen Solis <ssolis8619@gmail.com>
2021-01-19 22:22:54 -0600

    created functions for each question.  Takes user through the function to determine the correct or incorrect answer using an array

--- assets/script.js
+++ assets/script.js
@@ -6,14 +6,21 @@ var optionB = document.getElementById("optionB");
 var optionC = document.getElementById("optionC");
 var optionD = document.getElementById("optionD");
 var questionText = document.getElementById("quiz-question");
-
+var questionBank = ["2 + 2 ", "I ate "];
+var answerA = ["3 ", "answer"];
+var answerB = ["4 ", "blach"];
+var answerC = ["2 ", "answer"];
+var answerD = ["1 ", "answer"];
+var answersArray = ["4 ", "blah"];
+var userScore = 0;
+var timeLeft = 10;
 
 startBtn.addEventListener("click",initiateQuiz);
 
 
 function initiateQuiz() {
    
-    var timeLeft = 10;
+    
     var timeInterval = setInterval(function() {
       
     if(timeLeft !== 0){
@@ -29,14 +36,50 @@ function initiateQuiz() {
     optionC.style.display = "block";
     optionD.style.display = "block";
     startBtn.style.display = "none";
-    
+
+    questionOne();    
 }
 
-function nextQuestion(){
+function questionOne() {
+
+    questionText.textContent = questionBank[0];
+    optionA.textContent = answerA[0];
+    optionB.textContent = answerB[0];
+    optionC.textContent = answerC[0];
+    optionD.textContent = answerD[0];
     
+    function userAlert(event) {
+        
+        if (answersArray[0] === event.target.textContent) {
+            document.getElementById("indicator-div").textContent = "Your're Right";
+            userScore++;
+        }
+        else {
+            document.getElementById("indicator-div").textContent = "Your're Wrong";
+            timeLeft = timeLeft-5;
+        }
+        questionTwo();
+    }
+    
+
+    optionA.addEventListener("click", userAlert);
+    optionB.addEventListener("click", userAlert);
+    optionC.addEventListener("click", userAlert);
+    optionD.addEventListener("click", userAlert);
+
 
 }
 
-function answer(){
+
+
+function questionTwo() {
+
+    questionText.textContent = questionBank[1];
+    optionA.textContent = answerA[1];
+    optionB.textContent = answerB[1];
+    optionC.textContent = answerC[1];
+    optionD.textContent = answerD[1];
+
+    optionD.addEventListener("click", questionTwo);
 
 }
